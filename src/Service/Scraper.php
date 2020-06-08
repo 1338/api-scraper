fb56c3c80fb025e2ed42463515a0aaab19d7f724
1338 <maxvandersluis@yahoo.com>
2020-06-08 21:26:57 +0200

    Handle base slug

--- src/Service/Scraper.php
+++ src/Service/Scraper.php
@@ -2,13 +2,34 @@
 namespace Rootshell\ApiScraper;
 
 use GuzzleHttp\Client as HttpClient;
+use GuzzleHttp\Exception\GuzzleException;
 
-
+/**
+ * Class Scraper
+ * @package Rootshell\ApiScraper
+ */
 class Scraper
 {
     private $scraper;
+    private $baseSlug;
 
+    /**
+     * Scraper constructor.
+     * @param $startURL
+     * @param bool $access
+     * @param bool $secret
+     */
     public function __construct($startURL, $access = false, $secret = false) {
+        $baseSlug = parse_url($startURL, PHP_URL_PATH);
+
+        if(!empty($baseSlug)) {
+            if(!substr($baseSlug, -1, 1) == '/') {
+                $baseSlug .= '/';
+            }
+        }
+
+        $this->baseSlug = $baseSlug;
+
         $httpOptions = [
             'base_uri'  =>  $startURL,
             'auth'      =>  [],
@@ -23,11 +44,21 @@ class Scraper
         $this->scraper = new HttpClient($httpOptions);
     }
 
-    public function get($endpoint) {
-        return $this->scraper->get($endpoint);
-    }
-
-    public function post($endpoint, $options){
-        return $this->scraper->post($endpoint, $options);
+    /**
+     * @param string $type
+     * @param $endpoint
+     * @param $options
+     * @return bool|\Psr\Http\Message\ResponseInterface
+     */
+    public function request($type = 'GET', $endpoint, $options) {
+        try {
+            return $this->scraper->request(
+                $type,
+                "{$this->baseSlug}$endpoint",
+                $options
+            );
+        } catch (GuzzleException $e) {
+            return false;
+        }
     }
 }
